19baba3b52ae69a30e7325d219937652de0e81a5
John Diaz <nabetes2001@hotmail.com>
2023-12-09 15:42:21 -0500

    Update endpoint api

--- lib/utils/constants_app.dart
+++ lib/utils/constants_app.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ConstantsApp {
-  static const String mobileURL = "http://10.0.2.2:8080/api/";
-  static const String webURL = "http://localhost:8080/api/";
+  static const String webURL = "https://service-gestion-api-sena-a09533e6faf8.herokuapp.com/api/";
   static const int TIME_SPLASH_SCREEN = 5;
   static const double defaultPadding = 16.0;
 
